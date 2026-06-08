dbc97939b7f423894d0eaaab85fdbb784f44f3f2
Pratham Mittal <prathammittal189@gmail.com>
2026-06-08 10:03:50 +0530

    Time: 27 ms (73.33%), Space: 88.5 MB (13.26%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int storesRequired(vector<int>& quantities, int maxProducts) {
+        int stores = 0;
+        for (int q : quantities) {
+            stores += (q + maxProducts - 1) / maxProducts;
+        }
+        return stores;
+    }
+
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int low = 1;
+        int high = *max_element(quantities.begin(), quantities.end());
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int stores = storesRequired(quantities, mid);
+
+            if (stores > n) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
