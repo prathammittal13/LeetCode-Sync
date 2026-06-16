418faa9b4d8d990584cb4fca376284b0eb301bb8
Pratham Mittal <prathammittal189@gmail.com>
2026-06-16 10:16:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;  // Stack to store opening brackets
+        for (auto it : s) {
+            if (it == '(' || it == '{' || it == '[')
+                st.push(it);  // Push opening brackets to stack
+            else {
+                if (st.empty()) return false;  // No matching opening bracket
+                char ch = st.top();
+                st.pop();
+                if ((it == ')' && ch == '(') ||
+                    (it == ']' && ch == '[') ||
+                    (it == '}' && ch == '{'))
+                    continue;
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
